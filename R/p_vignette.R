9c295be5cdfa112e3791d8bfac97d9c900c84959
Tyler Rinker <tyler.rinker@gmail.com>
2012-05-31 10:43:36 -0400

    Attempted to address issue number 3: poor error message.  Also added Dason's suggestions for vignette search order.

--- R/p_vignette.R
+++ R/p_vignette.R
@@ -1,20 +1,32 @@
 p_vignette <-
 function(package = "all"){
     x <- as.character(substitute(package))
+    vigns <- vignette()
     if (x == "all") {
-        vignette() 
+        vigns     
     } else {
-        y <- vignette(package = x)$results[, 3]
-        if (length(y) > 1) {
-            w <- c(y, "All Vignettes")
-            z <- menu(w) 
-            if (w[z] == "All Vignettes") {
-                lapply(y, vignette)
-            } else {
-                vignette(w[z])
-            }
+        vn <- vigns$results
+        if (x %in% vn[, 3]) {
+            pn <- vn[vn[, 3] %in% x, 3]
+            vignette(pn)
         } else {
-            vignette(y)
+            if (!x %in% vn[, 1]){
+                stop (paste(x, 
+                            "does not appear to be a package with a vignette or the name of a vignette"))
+            } else {
+                y <- vignette(package = x)$results[, 3]
+                if (length(y) > 1) {
+                    w <- c(y, "All Vignettes")
+                    z <- menu(w) 
+                    if (w[z] == "All Vignettes") {
+                        lapply(y, vignette)
+                    } else {
+                        vignette(w[z])
+                    }
+                } else {
+                    vignette(y)
+                }
+            }
         }
     }
 }
