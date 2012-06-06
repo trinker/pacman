7e45880e00d864937de1b5a84883be6f755acdec
Dason Kurkiewicz <dasonk@iastate.edu>
2012-06-05 23:13:06 -0500

    Made p_library more robust and updated documentation.

--- R/p_library.R
+++ R/p_library.R
@@ -1,7 +1,9 @@
 #' Display library packages
 #'
 #' Generates a vector of all available add on packages 
-#' in the user's library and optionally opens that directory.
+#' available to the user and optionally opens the user's
+#' library (this isn't necessarily where all of the available
+#' packages are stored).
 #' 
 #' @rdname p_library
 #' @param open logical. If TRUE opens the directory of 
@@ -18,9 +20,8 @@ function (open = FALSE)
     ## This doesn't necessarily have all add-on
     ## packages though...
     userlibrary <- Sys.getenv("R_LIBS_USER")
-    userpackages <- list.files(userlibrary)
+    userpackages <- .packages(all.available = TRUE) #list.files(userlibrary)
     if (open) {
-
         p_opendir(userlibrary)
     }
     return(userpackages)
