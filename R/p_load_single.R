509a31a0f24a93333283fc409f7b6f206c58563d
Dason Kurkiewicz <dasonk@iastate.edu>
2012-09-29 17:00:38 -0500

    Removed unneccesary if/else

--- R/p_load_single.R
+++ R/p_load_single.R
@@ -7,10 +7,8 @@ function (package, install = TRUE){
     }
     # Loaded failed - attempt to install
     p_install(package, character.only = TRUE)
-    # Try to load again
-    if(require(package, character.only = TRUE)){
-        return(TRUE)
-    }else{
-        return(FALSE)
-    }
+    
+    # Try to load again - return results
+    require(package, character.only = TRUE)
+
 }
