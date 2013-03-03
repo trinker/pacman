76cc6d6f7742344e49ec9610845a0660cd205cad
Dason Kurkiewicz <dasonk@iastate.edu>
2013-03-02 21:23:17 -0600

    Overhaul of p_unload to have more readable names.

--- R/p_unload.R
+++ R/p_unload.R
@@ -4,7 +4,7 @@
 #' 
 #' @param \dots name of package(s) or "all" 
 #' (all removes all add on packages)
-#' @param negate logical. If TRUE will uninstall 
+#' @param negate logical. If TRUE will unload
 #' all add on packages except those provided to p_unload.
 #' @note p_unload will not unload the base install packages that load when R boots up.
 #' @seealso \code{\link[base]{detach}}
@@ -30,44 +30,49 @@
 p_unload <- 
 function (..., negate = FALSE) 
 {
-    mf <- match.call(expand.dots = FALSE)
-    y <- as.character(mf[[2]])
-    packs <- names(sessionInfo()[["otherPkgs"]])
-    if (negate & !any(y %in% "all")) {
-        y <- packs[!packs %in% y]
+    toUnload <- as.character(match.call(expand.dots = FALSE)[[2]])
+    loaded <- names(sessionInfo()[["otherPkgs"]])
+    if (negate & !any(toUnload %in% "all")) {
+        toUnload <- loaded[!loaded %in% toUnload]
     }
-    z <- y[!y %in% c(packs, "all")]
-    m <- getOption("defaultPackages")
-    o <- z[z %in% c("base", m)]
-    z <- z[!z %in% c("base", m)]
-    mt <- y %in% "all"
-    y <- y[y %in% packs]
+    
+    toUnloadButNotLoaded <- toUnload[!toUnload %in% c(loaded, "all")]
+    #previously m
+    defaultPackages <- getOption("defaultPackages")
+    toUnloadButCantUnload <- toUnloadButNotLoaded[toUnloadButNotLoaded %in% c("base", defaultPackages)]
+    toUnloadButNotLoaded <- toUnloadButNotLoaded[!toUnloadButNotLoaded %in% c("base", defaultPackages)]
+    mt <- toUnload %in% "all"
+    toUnload <- toUnload[toUnload %in% loaded]
     if (!identical(mt, logical(0)) & all(mt)) {
-        y <- names(sessionInfo()[["otherPkgs"]])
-        x <- paste0("package:", y)
-        y <- paste(y, collapse = ", ")
+        toUnload <- names(sessionInfo()[["otherPkgs"]])
+        x <- paste0("package:", toUnload)
+        toUnload <- paste(toUnload, collapse = ", ")
         invisible(lapply(x, function(package) {
             suppressWarnings(detach(package, character.only = TRUE, 
                 force = TRUE, unload = TRUE))
         }))
     } else {
-        unload <- function(y2) {
-            x <- paste0("package:", y2)
+        unload <- function(packageToUnload) {
+            x <- paste0("package:", packageToUnload)
             suppressWarnings(detach(x, unload = TRUE, character.only = TRUE, 
                 force = TRUE))
         }
-        invisible(lapply(y, unload))
+        invisible(lapply(toUnload, unload))
     }
-    if (!identical(o, character(0))) {
-        cat(paste0("The following packages are a base ", "install and will not be unloaded:\n"), 
-            paste0("\b", paste(o, collapse = ", ")), "\n\n")
+    if (!identical(toUnloadButCantUnload, character(0))) {
+        message(paste0("The following packages are a base ", 
+                   "install and will not be unloaded:\n"), 
+            paste0("\b", paste(toUnloadButCantUnload, collapse = ", ")), 
+            "\n\n")
     }
-    if (!identical(z, character(0))) {
-        cat("The following packages were not previously loaded:\n", 
-            paste0("\b", paste(z, collapse = ", ")), "\n\n")
+    if (!identical(toUnloadButNotLoaded, character(0))) {
+        message("The following packages were not previously loaded:\n", 
+            paste0("\b", paste(toUnloadButNotLoaded, collapse = ", ")), 
+            "\n\n")
     }
-    if (length(y) > 0) {
-        cat("The following packages have been unloaded:\n", paste0("\b", 
-            paste(y, collapse = ", ")), "\n")
+    if (length(toUnload) > 0) {
+        message("The following packages have been unloaded:\n",
+            paste0("\b", paste(toUnload, collapse = ", ")), 
+            "\n")
     }
 }
