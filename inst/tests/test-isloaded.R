998444b6f794985b98f6637a49a0b499e7f018f9
Dason Kurkiewicz <dasonk@iastate.edu>
2013-08-31 23:24:47 -0500

    Fixed test message for test-isloaded

--- inst/tests/test-isloaded.R
+++ inst/tests/test-isloaded.R
@@ -1,6 +1,6 @@
 context("Checking p_isloaded")
 
-test_that("p_author works",{
+test_that("p_isloaded works",{
     require(pacman)
     expect_true(p_isloaded(pacman))
     expect_false(p_isloaded(asdfasdfasdf))
