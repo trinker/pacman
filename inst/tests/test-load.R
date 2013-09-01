07b937257f813e2356ace9e0bc038b3e88cc9bd5
Dason Kurkiewicz <dasonk@iastate.edu>
2013-08-31 23:23:36 -0500

    Added much needed tests for p_load

---
+++ inst/tests/test-load.R
@@ -0,0 +1,30 @@
+context("Checking p_load")
+
+test_that("p_load works for a single string",{
+
+    # Hopefully MASS is available...
+    expect_true(p_load("MASS"))
+    expect_true(p_load("pacman"))
+})
+
+test_that("p_load works for a single non-string input",{
+    expect_true(p_load(MASS))
+    expect_true(p_load(pacman))
+})
+
+test_that("p_load works for a character vector",{
+    success <- c(MASS=TRUE, pacman=TRUE)
+    packs <- c("MASS", "pacman")
+    expect_equal(p_load(packs, character.only = TRUE), success)
+    expect_equal(p_load(c("MASS", "pacman"), character.only = TRUE), success)
+})
+
+test_that("p_load works for individual string inputs", {
+    success <- c(MASS=TRUE, pacman=TRUE)
+    expect_equal(p_load("MASS", "pacman"), success)
+})
+
+test_that("p_load works for individual non-string inputs", {
+    success <- c(MASS=TRUE, pacman=TRUE)
+    expect_equal(p_load(MASS, pacman), success)
+})
