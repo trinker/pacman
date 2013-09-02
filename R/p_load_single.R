trinker/pacman

commit aae81888c6ea2cc81c31eb67071685e2a42733b7

Fixed an issue where p_load ignores the install parameter.

R/p_load_single.R
@@ -7,9 +7,13 @@ function (package, install = TRUE, ...){
         return(TRUE)
     }
     # Loaded failed - attempt to install
-    p_install(package, character.only = TRUE, ...)
     
-    # Try to load again - return results
-    suppressMessages(require(package, character.only = TRUE))
+    if(install){
+        p_install(package, character.only = TRUE, ...) 
+        # Try to load again - return results
+        suppressMessages(require(package, character.only = TRUE))
+    }
 
+    # Couldn't load and we didn't want to install
+    return(FALSE)
 }

R/p_load.R
@@ -49,10 +49,12 @@ p_load <- function (..., install = TRUE, update = getOption("pac_update"), chara
     }
     
     # Attempt to load packages making note of which don't load
-    loaded <- sapply(packages, p_load_single)
+    loaded <- sapply(packages, p_load_single, install = install)
     # Give a warning if some packags couldn't be installed/loaded
     if(!all(loaded)){
         failed <- packages[!loaded]
+        # TODO: We should make this more descriptive
+        # Could it not load - or could it not install?
         warning("Failed to install/load:\n", paste(failed, collapse=", "))
     }
     

commit 2e0bee838d1a03347194e1f83f00d3abb02906d8

Added return statement to p_load_single.
This fixes a bug introduced in aae81888c6ea2cc81c31eb67071685e2a42733b7 and fixes issue #32

R/p_load_single.R
@@ -11,7 +11,7 @@ function (package, install = TRUE, ...){
     if(install){
         p_install(package, character.only = TRUE, ...) 
         # Try to load again - return results
-        suppressMessages(require(package, character.only = TRUE))
+        return(suppressMessages(require(package, character.only = TRUE)))
     }
 
     # Couldn't load and we didn't want to install