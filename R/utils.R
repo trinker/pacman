46d7e968dfcfb8d18ab06be9bb530ac32312e5ac
tyler rinker <tyler.rinker@gmail.com>
2014-01-08 22:56:37 -0500

    updated:
    - .Rbuildignore (remove logo png and docx)
    - DESCRIPTION Author to Authors
    - writeClipboard to NULL on non Windows

--- R/utils.R
+++ R/utils.R
@@ -19,12 +19,12 @@ left.just <- function(dataframe, column = NULL, keep.class = FALSE) {
         if (is.null(column)) column <- colnames(DF2)
         Q <- max(nchar(c(as.character(DF2[, column]), names(DF2)[column])))
         DF2 <- data.frame(rbind(colnames(DF2), do.call(cbind,
-                                                       lapply(DF2, as.character))), check.names = FALSE)
+            lapply(DF2, as.character))), check.names = FALSE)
         DF2[, column] <- left.j(as.character(DF2[, column]))     
         if (is.character(column)) {
             col <- names(DF2)[which(names(DF2) == column)]
             names(DF2)[which(names(DF2) == column)] <- sprintf(paste("%-", 
-                                                                     Q, "s", sep = ""), col)
+                Q, "s", sep = ""), col)
         } else {
             if (is.numeric(column)) {
                 col <- names(DF2)[column]
@@ -61,12 +61,12 @@ left.just <- function(dataframe, column = NULL, keep.class = FALSE) {
         colClasses <- function(d, colClasses) {
             colClasses <- rep(colClasses, len=length(d))
             d[] <- lapply(seq_along(d), function(i) switch(colClasses[i], 
-                                                           numeric=as.numeric(d[[i]]), 
-                                                           character=as.character(d[[i]]), 
-                                                           Date=as.Date(d[[i]], origin='1970-01-01'), 
-                                                           POSIXct=as.POSIXct(d[[i]], origin='1970-01-01'), 
-                                                           factor=as.factor(d[[i]]),
-                                                           as(d[[i]], colClasses[i]) ))
+                numeric=as.numeric(d[[i]]), 
+                character=as.character(d[[i]]), 
+                Date=as.Date(d[[i]], origin='1970-01-01'), 
+                POSIXct=as.POSIXct(d[[i]], origin='1970-01-01'), 
+                factor=as.factor(d[[i]]),
+                as(d[[i]], colClasses[i]) ))
             d
         }
         DF3 <- colClasses(DF3, CLASS)
@@ -81,6 +81,9 @@ paste0 <- function(..., collapse = NULL){
 
 writeToClipboard <- function(out){
     os <- p_detectOS()
+    if (os != "Windows") {
+        writeClipboard <- NULL
+    }  		
     if(os == "Windows"){
         writeClipboard(out, format = 1)
     }else if(os == "Darwin") {           
