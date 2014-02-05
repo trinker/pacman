4399021d98d71b6f48fbab4ec9fa33f0985d8f93
tyler rinker <tyler.rinker@gmail.com>
2014-02-04 22:59:00 -0500

    Updated description to avoid CRAN check for conflicting authors.  See: https://github.com/trinker/pacman/commit/a54c0ad8909e95db2e7b9072494151a51e3f9b42#commitcomment-5277148

--- inst/tests/test-author.R
+++ inst/tests/test-author.R
@@ -1,7 +1,8 @@
 context("Checking p_author")
 
 test_that("p_author works",{
-    expect_output(p_author(pacman), "Tyler Rinker & Dason Kurkiewicz")
+    expect_output(p_author(pacman), 
+    	"Tyler Rinker [aut, cre, ctb],\n  Dason Kurkiewicz [aut, ctb]")
     expect_output(p_author(MASS), "Brian Ripley")
     #expect_output(p_author(base), "R Core Team") # apparently changed at some point
 })
