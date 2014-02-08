d228cedfb8b8936c3a8cf83eb508908a26833cf9
tyler rinker <tyler.rinker@gmail.com>
2014-02-07 19:15:47 -0500

    p_install now installs zip/tar.gz files.  cloase issue #32

--- R/p_install.R
+++ R/p_install.R
@@ -7,19 +7,46 @@
 #' @param character.only logical.  If \code{TRUE} \code{\dots} is treated a 
 #' character string.
 #' @param \ldots Additional parameters to pass to \code{install.packages}.
+#' @param path The path to the directory that contains the package.  It is 
+#' convenient to set \code{download_path} in .Rprofile options to the downloads 
+#' directory.
 #' @keywords install package
 #' @seealso \code{\link[utils]{install.packages}}
 #' @export
 #' @examples
 #' \dontrun{p_install(pacman)}
 p_install <-
-function(package, character.only = FALSE, ...){
-    p_set_cranrepo()
-    
+function(package, character.only = FALSE, path = getOption("download_path"), ...){
+
     if(!character.only){
         package <- as.character(substitute(package))
     }
-    install.packages(package, ...)
+
+    ## Detect if package ends in zip/tar.gz
+    if (grepl("\\.tar\\.gz|\\.zip", package)) {
+
+        if (file.exists(package)) {
+            tar_path <- package
+        } else {
+            if (file.exists(file.path(path, package))) {
+                tar_path <- file.path(path, package)
+            } else {
+                message("Package not found. Please select file...")
+                tar_path <- file.choose()
+            }      
+        }
+        install.packages(tar_path, repos = NULL, type = "source", ...)
+    } else {
+      
+        p_set_cranrepo()
+
+        if (p_loaded(char = package)) {
+            p_unload(char = package)
+        }
+
+        install.packages(package, ...)
+    }
+    message(paste("\n", package, "installed"))
 }
 
 #' @rdname p_install
