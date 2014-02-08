120612962666d32fbc2f78c445e05d53e5ba1039
tyler rinker <tyler.rinker@gmail.com>
2014-02-08 12:37:40 -0500

    Added the install family: `p_install_github`, `p_install_cran`, and `p_install_source`
    Also added a checker to check the install type/location called `p_source_type`

--- R/p_cran.R
+++ R/p_cran.R
@@ -1,16 +1,20 @@
 #' CRAN Packages
 #' 
-#' Generate a vector of all available packages.
+#' \code{p_cran} - Generate a vector of all available CRAN packages.
 #' 
 #' @param menu logical.  If \code{TRUE} allows user to select the package and 
 #' return that package name.
 #' @seealso \code{\link[utils]{available.packages}}
 #' @keywords CRAN package
+#' @rdname p_cran
 #' @export
 #' @examples
 #' \dontrun{
 #' p_cran()
 #' p_cran(TRUE)
+#' 
+#' p_iscran(ggplot2, folks_I_am_not_on_CRAN)
+#' p_iscran(char=c("ggplot2", "folks_I_am_not_on_CRAN"))
 #' }
 p_cran <-
 function(menu = FALSE){
@@ -22,3 +26,33 @@ function(menu = FALSE){
     
     return(b)          
 }
+
+#' CRAN Packages
+#' 
+#' \code{p_iscran} - Generate a logical vector determining if a package(s) is 
+#' found on CRAN.
+#' 
+#' @param \ldots name(s) of package(s).
+#' @param char Character vector containing packages to load.  If you are calling
+#' \code{p_iscran} from within a function (or just having difficulties calling it 
+#' using a character vector input) then pass your character vector of packages 
+#' to this parameter directly.
+#' @param character.only logical. If \code{TRUE} then \code{p_iscran} will only 
+#' accept a single input which is a character vector containing the names of 
+#' packages to load.
+#' @rdname p_cran
+#' @export
+p_iscran <- function(..., char, character.only = FALSE) {
+
+    if(!missing(char)){
+        packages <- char
+    }else if(character.only){
+        packages <- eval(match.call()[[2]])
+    }else{
+        packages <- as.character(match.call(expand.dots = FALSE)[[2]])
+    }
+    out <- packages %in% p_cran(FALSE)
+    names(out) <- packages
+    out
+}
+
--- R/p_install.R
+++ R/p_install.R
@@ -59,3 +59,144 @@ function(package, character.only = FALSE, path = getOption("download_path"), ...
 #' @rdname p_install
 #' @export
 p_get <- p_install
+
+
+## Attempts to Install a Package From CRAN
+## 
+## Attempts to install package from CRAN and returns logical success.
+## 
+## @param package A CRAN package name.
+## @param \ldots Other arguments passed to \code{\link[utils]{install.packages}}
+## @return Invisibly returns logical value of success after attempting to install.
+## @seealso \code{\link[utils]{install.packages}}
+## @examples
+## \dontrun{p_install_cran("pacman")}
+p_install_cran <- function(package, ...) {
+
+    p_set_cranrepo()
+
+    if (p_loaded(char = package)) {
+        p_unload(char = package)
+    }
+
+    ## Handling if no package supplied
+    if (length(package) == 1 && package == "") {
+        package <- NULL 
+    } 
+
+    ## Install and give TRUE/FALSE if package could be installed
+    warns <- function(x) x
+    outcome <- tryCatch(install.packages(package, ...), condition=warns) 
+    out <- ifelse(inherits(outcome, "simpleWarning"), FALSE, TRUE)
+    names(out) <- package
+    return(invisible(out))
+
+}
+
+## Attempts to Install a Package From tar.gz/.zip
+## 
+## Attempts to install package from tar.gz/.zip and returns logical success.
+## 
+## @param package A quoted character string of the name/partial name of the 
+## package to install as zip/tar.
+## @param path The path to the directory that contains the package.  It is 
+## convenient to set \code{download_path} in .Rprofile options to the downloads 
+## directory.
+## @param \ldots Other arguments passed to \code{\link[utils]{install.packages}}
+## @return Invisibly returns logical value of success after attempting to install.
+## @seealso \code{\link[utils]{install.packages}}
+p_install_source <- function(package, path = getOption("download_path"), ...) {
+
+    type <- p_source_type(package)
+
+    if (type == "source_downloads") {
+        package <- file.path(path, package)
+    } 
+
+    ## Install and give TRUE/FALSE if package could be installed
+    warns <- function(x) x
+    outcome <- tryCatch(install.packages(package, repos = NULL, 
+        type = "source", ...), condition=warns) 
+    out <- ifelse(inherits(outcome, "simpleWarning"), FALSE, TRUE)
+    names(out) <- package
+    return(invisible(out))
+
+}
+
+## Attempts to Install a Package From GitHub
+## 
+## Attempts to install package from GitHub and returns logical success.
+## 
+## @param repo Repository address in the format 
+## \code{[username/]repo[/subdir][@ref|#pull]}. Alternatively, you can specify 
+## \code{username}, \code{subdir}, \code{ref} or \code{pull} using the 
+## respective parameters (see \code{\link[devtools]{install_github}}); if both 
+## is specified, the values in \code{repo} take precedence.
+## @param \ldots Other arguments passed to \code{\link[devtools]{install_github}}
+## @return Invisibly returns logical value of success after attempting to install.
+## @examples
+## \dontrun{p_install_github("pacman")}
+p_install_github <- function(repo, ...) {
+
+    require(devtools)
+
+    ## Install and give TRUE/FALSE if package could be installed
+    outcome <- try(install_github(repo = repo, ...))
+    out <- ifelse(inherits(outcome, "try-error"), FALSE, TRUE)
+    names(out) <- package
+    return(invisible(out))
+
+}
+
+## Helper function for p_install.  Not exporting or documenting for now
+##
+## Check the Type/Location of a Package
+## 
+## Checks if a package is a local zip/tar.gz (\code{"source"}), on CRAN 
+## (\code{"cran"}), or GitHub (\code{"github"}).
+## 
+## @param package The package to check the type/location of.
+## @return Returns a character vector of package source type: \code{c("source", 
+## "cran", "github")}.
+## @note If package is not course or CRAN then source is assumed.
+## @examples
+## p_source_type("tm")
+p_source_type <- function(package, path = getOption("download_path")) {
+
+    ## Check if file is zip/tar.gz
+    zip_check <- grepl("\\.tar\\.gz|\\.zip", package)
+
+    ## Further checks of zip/tar.gz to make sure file exists
+    if (zip_check) {
+
+        ## Check if zip/tar.gz is local
+        zip_loc_check <- file.exists(package)
+        if (zip_loc_check) return("source")
+
+        if (!is.null(path)) {
+            ## Check if zip/tar.gz is local in downloads
+            zip_download_check <- file.exists(file.path(path, package))
+            if (zip_download_check) return("source_downloads")
+        }
+
+        ## Check isf zip/tar.gz is web based
+        require(RCurl)
+        zip_web_check <- url.exists(package)
+        if (zip_web_check) {
+            return("source") 
+        } else {
+            message(sprintf("%s can not be located", package))
+            return(invisible(NA))
+        }
+    }
+
+    ## Check if CRAN
+    cran_check <- p_iscran(char=package)
+    if (cran_check) return("cran")
+
+    ## If not found github assumed
+    ## I don't like this and would like logical checking in future
+    return("github")
+
+}
+
