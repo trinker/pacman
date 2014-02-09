73b2adf8a00f5a1833f8d738152355673dfd476f
tyler rinker <tyler.rinker@gmail.com>
2014-02-08 22:39:28 -0500

    changed testthat to represent changes to p_load

--- inst/tests/test-load.R
+++ inst/tests/test-load.R
@@ -3,30 +3,29 @@ context("Checking p_load")
 test_that("p_load works for a single string",{
 
     # Hopefully MASS is available...
-    expect_true(p_load("MASS"))
-    expect_true(p_load("pacman"))
+    expect_true(p_load("MASS")[["loaded"]] == "MASS")
+    expect_true(p_load("pacman")[["loaded"]] == "pacman")
 })
 
 test_that("p_load works for a single non-string input",{
-    expect_true(p_load(MASS))
-    expect_true(p_load(pacman))
+    expect_true(p_load(MASS)[["loaded"]] == "MASS")
+    expect_true(p_load(pacman)[["loaded"]] == "pacman")
 })
 
 
 test_that("p_load works for a character vector", {
-    success <- c(MASS=TRUE, pacman=TRUE)
     packs <- c("MASS", "pacman")
     
-    expect_equal(p_load(char = packs), success)
-    expect_equal(p_load(c("MASS", "pacman"), character.only = TRUE), success)
+    expect_equal(p_load(char = packs)[["loaded"]], packs)
+    expect_equal(p_load(c("MASS", "pacman"), character.only = TRUE)[["loaded"]], packs)
 })
 
 test_that("p_load works for individual string inputs", {
-    success <- c(MASS=TRUE, pacman=TRUE)
-    expect_equal(p_load("MASS", "pacman"), success)
+    success <- c("MASS", "pacman")
+    expect_equal(p_load("MASS", "pacman")[["loaded"]], success)
 })
 
 test_that("p_load works for individual non-string inputs", {
-    success <- c(MASS=TRUE, pacman=TRUE)
-    expect_equal(p_load(MASS, pacman), success)
+    success <- c("MASS", "pacman")
+    expect_equal(p_load(MASS, pacman)[["loaded"]], success)
 })
