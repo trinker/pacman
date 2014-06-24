04fbe3251ef7bfd7e9f919cdc4a8f009ba335e24
Dason Kurkiewicz <dasonk@iastate.edu>
2014-06-23 21:48:48 -0500

    Use lattice instead of MASS

--- tests/testthat/test-author.R
+++ tests/testthat/test-author.R
@@ -2,6 +2,5 @@ context("Checking p_author")
 
 test_that("p_author works",{
     expect_output(p_author(pacman), "Tyler Rinker \\[aut, cre, ctb\\], Dason Kurkiewicz \\[aut, ctb\\]")
-    expect_output(p_author(MASS), "Brian Ripley")
-    #expect_output(p_author(base), "R Core Team") # apparently changed at some point
+    expect_output(p_author(lattice), "Deepayan Sarkar <deepayan.sarkar@r-project.org>")
 })
