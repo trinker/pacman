f296eece94d5cfae1067245ee697aa2a61c87a33
Dason Kurkiewicz <dasonk@iastate.edu>
2014-06-23 21:33:08 -0500

    Made parameters more explicit

--- tests/testthat/test-exists.R
+++ tests/testthat/test-exists.R
@@ -2,12 +2,12 @@ context("Checking p_exists")
 
 test_that("p_exists works for local packages",{
 	
-	expect_true(p_exists(pacman, FALSE))
-    expect_false(p_exists(thisisnotarealpackage235asdkjlakl3lkj), FALSE)
+	expect_true(p_exists(pacman, cran = FALSE))
+    expect_false(p_exists(thisisnotarealpackage235asdkjlakl3lkj, cran = FALSE))
 })
 
 test_that("p_exists works for packages on CRAN",{
 	
-    expect_true(p_exists(ggplot2))
-    expect_false(p_exists(thisisnotarealpackage235asdkjlakl3lkj))
+    expect_true(p_exists(ggplot2, cran = TRUE))
+    expect_false(p_exists(thisisnotarealpackage235asdkjlakl3lkj, cran = TRUE))
 })
