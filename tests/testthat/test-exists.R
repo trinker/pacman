eb47c22a04e58f10a5d54c0054231895e38a426d
Tyler Rinker <tyler.rinker@gmail.com>
2014-10-21 23:08:00 -0400

    fixed failing test vignette taken care of #56

--- tests/testthat/test-exists.R
+++ tests/testthat/test-exists.R
@@ -2,14 +2,14 @@ context("Checking p_exists")
 
 test_that("p_exists works for local packages",{
 	
-	expect_true(p_exists(pacman, cran = FALSE))
-    expect_false(p_exists(thisisnotarealpackage235asdkjlakl3lkj, cran = FALSE))
+	expect_true(p_exists(pacman, local = TRUE))
+    expect_false(p_exists(thisisnotarealpackage235asdkjlakl3lkj, local = TRUE))
 })
 
 test_that("p_exists works for packages on CRAN",{
 	
-    expect_true(p_exists(ggplot2, cran = TRUE))
-    expect_false(p_exists(thisisnotarealpackage235asdkjlakl3lkj, cran = TRUE))
+    expect_true(p_exists(ggplot2, local = TRUE))
+    expect_false(p_exists(thisisnotarealpackage235asdkjlakl3lkj, local = TRUE))
 })
 
 test_that("p_isinstalled works", {
