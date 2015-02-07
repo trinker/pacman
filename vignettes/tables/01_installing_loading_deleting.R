6bc0309f12d7c3b5944577acf9b0e9a7f8f1a9ca
Tyler <tyler.rinker@gmail.com>
2015-02-07 13:38:27 -0500

    updated table in vignette

--- vignettes/tables/01_installing_loading_deleting.R
+++ vignettes/tables/01_installing_loading_deleting.R
@@ -1,10 +1,10 @@
 | pacman Function | Base Equivalent | Description |
 |----------------------|----------------------|----------------|
-| `p_load`  |  `install.packages` + `library` | Loads and Install Packages |
+| `p_load`  |  `install.packages` + `library` | Load and Install Packages |
 | `p_install` |  `install.packages` | Install Packages from CRAN |
-| `p_load_gh`  |  NONE | Loads and Install GitHub Packages |
+| `p_load_gh`  |  NONE | Load and Install GitHub Packages |
 | `p_install_gh` |  NONE | Install Packages from GitHub |    
 | `p_install_version` |  `install.packages` & <br/> `packageVersion` | Install Minimum Version of Packages |
 | `p_temp` |  NONE | Install a Package Temporarily |
-| `p_unload` | `detach` | Unloads Packages from the Search Path |
+| `p_unload` | `detach` | Unload Packages from the Search Path |
 | `p_update` | `update.packages` | Update Out-of-Date Packages |
