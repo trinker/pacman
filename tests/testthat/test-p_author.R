84c15660a5d7a22e7377aebcf19551b7f6ceb78e
Tyler <tyler.rinker@gmail.com>
2015-05-05 18:11:32 -0400

    updated test to work with the new testthat.  This may be an issue that requires a new push to CRAN

--- tests/testthat/test-p_author.R
+++ tests/testthat/test-p_author.R
@@ -1,6 +1,10 @@
 context("Checking p_author")
 
 test_that("p_author works",{
-    expect_output(p_author(pacman), "Tyler Rinker \\[aut, cre, ctb\\], Dason Kurkiewicz \\[aut, ctb\\], Keith Hughitt \\[ctb\\]")
+    expect_equal(
+        strsplit(p_author(pacman), "(?<=],)\\s|\\n", perl=TRUE)[[1]],
+        c("Tyler Rinker [aut, cre, ctb],", "Dason Kurkiewicz [aut, ctb],", 
+            "Keith Hughitt [ctb]")
+    )
     expect_output(p_author(lattice), "Deepayan Sarkar <deepayan.sarkar@r-project.org>")
 })
