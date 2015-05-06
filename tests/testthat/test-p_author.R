a9f1c6c7774a3f6323d8db06db05bb7d24be2bc0
Tyler <tyler.rinker@gmail.com>
2015-05-05 20:27:50 -0400

    altered p_author test to use grepl.  This passes with both new/old testthat.

--- tests/testthat/test-p_author.R
+++ tests/testthat/test-p_author.R
@@ -1,10 +1,9 @@
 context("Checking p_author")
 
 test_that("p_author works",{
-    expect_equal(
-        strsplit(p_author(pacman), "(?<=],)\\s|\\n", perl=TRUE)[[1]],
-        c("Tyler Rinker [aut, cre, ctb],", "Dason Kurkiewicz [aut, ctb],", 
-            "Keith Hughitt [ctb]")
+    expect_true(
+        grepl("Tyler Rinker \\[aut, cre, ctb\\].+?Dason Kurkiewicz \\[aut, ctb\\]", 
+            p_author(pacman))
     )
     expect_output(p_author(lattice), "Deepayan Sarkar <deepayan.sarkar@r-project.org>")
 })
