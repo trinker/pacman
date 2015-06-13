e69d657735c3e398032b3bb141c3d90f1a71f162
Tyler <tyler.rinker@gmail.com>
2015-06-13 15:36:42 -0400

    fixed unit test p_version

--- tests/testthat/test-p_version.R
+++ tests/testthat/test-p_version.R
@@ -2,6 +2,6 @@ context("Checking p_version")
 
 test_that("p_version works",{
     require(lattice)
-    expect_equal(p_version(), R.Version()[["version.string"]])
+    expect_equal(p_version(), getRversion())
     expect_true(p_ver("lattice") >= "0.20.29")
 })
