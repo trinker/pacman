357f0a53e1a2ebc0297ef8ec970f76dc03a10693
Dason Kurkiewicz <dasonk@gmail.com>
2017-07-19 10:11:43 -0400

    Added an actual test to see if p_install_gh could install from github. Chose Dasonk/fss because it has no dependencies and is small.

--- tests/testthat/test-p_install_gh.R
+++ tests/testthat/test-p_install_gh.R
@@ -3,4 +3,16 @@ context("Checking p_install_gh")
 test_that("p_install_gh works",{
     require(pacman)
     expect_false(p_install_gh("greg/iDontExistAnywhere"))
+    
+    fss_exist <- FALSE
+    if(p_exists("fss")){
+        fss_exist <- TRUE
+        p_delete("fss")
+    }
+    
+    expect_true(p_install_gh("Dasonk/fss"))
+    if(!fss_exist){
+        p_delete("fss")
+    }
+    
 })
