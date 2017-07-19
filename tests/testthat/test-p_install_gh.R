b27111eee42742847c57c972547669cbab8d5bc4
Dason Kurkiewicz <dasonk@gmail.com>
2017-07-19 10:15:57 -0400

    Added Comments

--- tests/testthat/test-p_install_gh.R
+++ tests/testthat/test-p_install_gh.R
@@ -1,9 +1,18 @@
 context("Checking p_install_gh")
 
 test_that("p_install_gh works",{
-    require(pacman)
+    
+    # Suppress Warnings in case it was built on a different version
+    # of R and we're running the tests...
+    suppressWarnings(require(pacman))
+    
     expect_false(p_install_gh("greg/iDontExistAnywhere"))
     
+    # We don't want the package installed when testing
+    # if we can install it.  If it is installed
+    # we don't want to remove it after the test
+    # but if it isn't installed we don't want to
+    # keep it on the user's system.
     fss_exist <- FALSE
     if(p_exists("fss")){
         fss_exist <- TRUE
@@ -11,6 +20,8 @@ test_that("p_install_gh works",{
     }
     
     expect_true(p_install_gh("Dasonk/fss"))
+    
+    # Cleanup
     if(!fss_exist){
         p_delete("fss")
     }
